0f3ef67859eadd6da857e53db4004b32cbd0f606
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-16 22:40:43 +0530

    add:RouterProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Home from "./pages/Home";
+import AppLayout from "./layout/AppLayout";
 
 function App() {
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<AppLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  ));
   return (
-    <h1>App</h1>
+    <RouterProvider router={router} />
   );
 }
 
