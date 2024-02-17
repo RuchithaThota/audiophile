890fc783618e99e56864dbc1c0a306ef70409818
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-17 11:03:10 +0530

    remove:comments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   ];
   //JSX
   return (
-    <div className="bg-neutral-950 md:px-10">
+    <div className="bg-custom-gray md:px-10">
       <div
         className="h-[90px] w-full max-w-[1109px] mx-auto my-0 flex items-center 
     justify-center relative border-b border-gray-800 
@@ -32,14 +32,16 @@ function Header() {
         <ul className="hidden lg:flex lg:justify-between lg:items-center lg:gap-8">
           {navLinks.map(([title, url]) => {
             return (
-              <Link
-                to={url}
-                className="
-    text-zinc-50 font-manrope text-[13px]  font-bold uppercase 
-    transition-colors ease-in-out duration-300 hover:text-orange-400  "
-              >
-                {title}
-              </Link>
+              <li>
+                <Link
+                  to={url}
+                  className="
+    text-custom-white font-manrope text-[13px]/6 font-semibold uppercase 
+    transition-colors ease-in-out duration-300 hover:text-orange-400"
+                >
+                  {title}
+                </Link>
+              </li>
             );
           })}
         </ul>
@@ -52,50 +54,3 @@ function Header() {
 }
 
 export default Header;
-
-{
-  /* <div className="bg-neutral-950">
-<div
-  className="h-[90px] w-full max-w-[768px] mx-auto my-0 flex items-center 
-justify-center relative border-b border-gray-800 
-md:
-"
->
-  <button className="absolute left-8">
-    <img src={menuIcon} alt="menuIcon" />
-  </button>
-  <img src={logo} alt="logo" />
-  <button className="absolute right-8">
-    <img src={cartIcon} alt="cartIcon" />
-  </button>
-</div>
-</div> */
-}
-
-{
-  /* <div className="flex-1">
-<img src={menuIcon} alt="menu" className="cursor-pointer lg:hidden" />
-<img src={logo} alt="logo" className="cursor-pointer " />
-</div>
-<ul className="flex-1 flex justify-between items-center">
-{navLinks.map(([title, url]) => {
-  return (
-    <Link
-      to={url}
-      className="
-    text-zinc-50 font-manrope text-[12px] font-semibold uppercase 
-    transition-colors ease-in-out duration-300 hover:text-orange-400"
-    >
-      {title}
-    </Link>
-  );
-})}
-</ul>
-<div className="flex-1">
-<img
-  src={cartIcon}
-  alt="cartIcon"
-  className="ml-auto cursor-pointer"
-/>
-</div> */
-}
