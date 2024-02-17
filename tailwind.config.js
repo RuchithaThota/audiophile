1905ed60b9d7c0cfe7570c5d21f357deedbb3a32
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-17 11:01:42 +0530

    add:custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
       fontFamily: {
         'manrope': ['Manrope', 'sans-serif'],
       },
+      colors: {
+        'custom-gray': '#101010',
+        'custom-white': "#fafafa",
+        'custom-white-2': "#fafafa80",
+        'footer-line': '#d87d4a'
+      },
     },
   },
   plugins: [],
