b5190d0b9856c5efe1a53a5079df8b8ce9318e65
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-18 22:22:16 +0530

    change:import path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FeaturedProduct from "../components/Home/FeaturedProduct";
 import Categories from "../components/Category/Categories";
-import HomeProducts from "../components/Home/HomeProducts";
+import HomeProducts from "../components/Home/HomeProducts/HomeProducts";
 import AboutUs from "../components/AboutUs";
 
 function Home() {
@@ -9,8 +9,10 @@ function Home() {
     <div>
       <FeaturedProduct />
       <div
-        className="bg-custome-white px-6 pt-10 pb-20 md:px-10 md:py-24
-      lg:pt-[120px] lg:px-10 lg:pb-[200px]  "
+        className="bg-custome-white 
+        w-full max-w-[1109px] mx-auto
+        px-6 pt-10 pb-20 md:px-10 md:py-24
+      lg:pt-[120px] lg:px-10 lg:pb-[200px] flex flex-col gap-28 "
       >
         <Categories />
         <HomeProducts />
