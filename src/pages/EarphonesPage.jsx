1df2368e6675a1bbba0157792581b2697dac709a
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 18:01:08 +0530

    split component as react-router-dom Outlet

--- src/pages/EarphonesPage.jsx
+++ src/pages/EarphonesPage.jsx
@@ -1,23 +1,10 @@
 import React from "react";
-import Categories from "../components/Categories";
-import AboutUs from "../components/AboutUs";
-import CategoryTitle from "../components/CategoryTitle";
 import EarphoneOne from "../components/Earphones/EarphoneOne";
 
 function EarphonesPage() {
   return (
     <div>
-      <CategoryTitle title={"Earphones"} />
-      <div
-        className="bg-custome-white 
-        w-full max-w-[1200px] mx-auto
-        px-6 pt-10 pb-20 md:px-10 md:py-24
-      lg:pt-[120px] lg:px-10 lg:pb-[200px] flex flex-col gap-28 "
-      >
-        <EarphoneOne />
-        <Categories />
-        <AboutUs />
-      </div>
+      <EarphoneOne />
     </div>
   );
 }
