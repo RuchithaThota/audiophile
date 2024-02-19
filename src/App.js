387a6bce10916a482026e598bf14e1686a3732b5
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 14:08:38 +0530

    add:New Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
 import Home from "./pages/Home";
 import AppLayout from "./layout/AppLayout";
+import HeadphonesPage from "./pages/HeadphonesPage";
+import EarphonesPage from "./pages/EarphonesPage";
+import SpeakersPage from "./pages/SpeakersPage";
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<AppLayout />}>
       <Route index element={<Home />} />
+      <Route path="headphones" element={<HeadphonesPage />} />
+      <Route path="earphones" element={<EarphonesPage />} />
+      <Route path="speakers" element={<SpeakersPage />} />
     </Route>
   ));
   return (
