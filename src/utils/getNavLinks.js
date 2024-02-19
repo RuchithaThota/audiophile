f11d22b9d113285c6f3f9c8e722260ca0032e06c
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 17:58:16 +0530

    getNavLinks utility

---
+++ src/utils/getNavLinks.js
@@ -0,0 +1,9 @@
+export const getNavLinks = () => {
+    const navLinks = [
+        ["Home", "/"],
+        ["Headphones", "/category/headphones"],
+        ["Speakers", "/category/speakers"],
+        ["Earphones", "/category/earphones"],
+    ];
+    return navLinks;
+}
