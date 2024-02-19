a3a461594b0bf1770a74e2eaf7f9e2dbe81d8554
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 21:48:39 +0530

    add: lazy importing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
+import React, { Suspense, lazy } from 'react';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import AppLayout from "./layout/AppLayout";
-import HomePage from "./pages/HomePage";
-import CategoryLayout from "./layout/CategoryLayout";
-import HeadphonesPage from "./pages/HeadphonesPage";
-import EarphonesPage from "./pages/EarphonesPage";
-import SpeakersPage from "./pages/SpeakersPage";
+import Shimmer from './Shimmer/Shimmer';
+import ErrorPage from './pages/ErrorPage';
+const AppLayout = React.lazy(() => import("./layout/AppLayout"));
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const CategoryLayout = lazy(() => import("./layout/CategoryLayout"));
+const HeadphonesPage = lazy(() => import("./pages/HeadphonesPage"));
+const EarphonesPage = lazy(() => import("./pages/EarphonesPage"));
+const SpeakersPage = lazy(() => import("./pages/SpeakersPage"));
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<AppLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="category" element={<CategoryLayout />} >
-        <Route path="headphones" element={<HeadphonesPage />} />
-        <Route path="earphones" element={<EarphonesPage />} />
-        <Route path="speakers" element={<SpeakersPage />} />
+    <Route path="/"
+      element={<Suspense fallback={<Shimmer />}><AppLayout /></Suspense>}
+      errorElement={<ErrorPage />}
+    >
+      <Route index
+        element={<Suspense fallback={<Shimmer />}><HomePage /></Suspense>} />
+      <Route path="category"
+        element={<Suspense fallback={<Shimmer />}><CategoryLayout /></Suspense>} >
+        <Route path="headphones"
+          element={<Suspense fallback={<Shimmer />}><HeadphonesPage /></Suspense>} />
+        <Route path="earphones"
+          element={<Suspense fallback={<Shimmer />}><EarphonesPage /></Suspense>} />
+        <Route path="speakers"
+          element={<Suspense fallback={<Shimmer />}><SpeakersPage /></Suspense>} />
       </Route>
     </Route>
   ));
