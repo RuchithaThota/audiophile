d1bc39427d7e629e40be8873ea2abebf66654af9
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 10:24:46 +0530

    feat:HomeProductThree

--- src/components/Home/HomeProducts/HomeProducts.jsx
+++ src/components/Home/HomeProducts/HomeProducts.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./HomeProducts.css";
 import HomeProductOne from "./HomeProductOne";
 import HomeProductTwo from "./HomeProductTwo";
+import HomeProductThree from "./HomeProductThree";
 
 function HomeProducts() {
   return (
     <div className="w-full flex flex-col gap-10">
       <HomeProductOne />
       <HomeProductTwo />
+      <HomeProductThree />
     </div>
   );
 }
