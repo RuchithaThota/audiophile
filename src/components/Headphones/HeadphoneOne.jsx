8028258d86d1eb5334c1cad52acafc595f70b280
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 16:19:11 +0530

    feat:responsive design

--- src/components/Headphones/HeadphoneOne.jsx
+++ src/components/Headphones/HeadphoneOne.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import desktopXx59 from "../../assets/images/category-headphones/desktop/image-xx59.jpg";
+import tabletXx59 from "../../assets/images/category-headphones/tablet/image-xx59.jpg";
+import mobileXx59 from "../../assets/images/category-headphones/mobile/image-xx59.jpg";
+import { Link } from "react-router-dom";
 
 function HeadphoneOne() {
   return (
-    <div>HeadphoneOne</div>
-  )
+    <div className="lg:flex lg:items-center">
+      <img
+        src={mobileXx59}
+        alt="xx59 Headphones"
+        className="rounded-lg mb-8 md:hidden"
+      />
+      <img
+        src={tabletXx59}
+        alt="xx59 Headphones"
+        className="hidden md:block md:mb-[52px] md:rounded-lg lg:hidden"
+      />
+      <img
+        src={desktopXx59}
+        alt="xx59 Headphones"
+        className="hidden lg:block lg:rounded-lg lg:w-1/2"
+      />
+      <div
+        className="w-full flex flex-col items-center gap-6 text-center 
+      md:w-[572px] md:mx-auto md:my-0 lg:items-start lg:text-left lg:pl-32 lg:w-1/2"
+      >
+        <h2
+          className="font-bold text-[28px] md:text-[40px] text-black uppercase 
+        md:w-[360px]"
+        >
+          XX59 HEADPHONES
+        </h2>
+        <p className="font-medium text-[15px]/7 text-custom-gray-2">
+          Enjoy your audio almost anywhere and customize it to your specific
+          tastes with the XX59 headphones. The stylish yet durable versatile
+          wireless headset is a brilliant companion at home or on the move.
+        </p>
+        <Link
+          to=""
+          className="font-bold text-[13px] text-white bg-primary uppercase 
+          h-12 px-10 flex items-center"
+        >
+          See Product
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default HeadphoneOne
+export default HeadphoneOne;
