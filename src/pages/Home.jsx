711edad7a19cfe2bdc0795e4200bfc7b0f7ec95d
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-19 10:25:28 +0530

    change max-width

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
       <FeaturedProduct />
       <div
         className="bg-custome-white 
-        w-full max-w-[1109px] mx-auto
+        w-full max-w-[1200px] mx-auto
         px-6 pt-10 pb-20 md:px-10 md:py-24
       lg:pt-[120px] lg:px-10 lg:pb-[200px] flex flex-col gap-28 "
       >
