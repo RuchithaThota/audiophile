5cd8b0b143f356c108a621794896c31df7f7288c
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-23 21:58:07 +0530

    add:route for product

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import {
   Navigate, Route, RouterProvider, createBrowserRouter, createRoutesFromElements,
 } from "react-router-dom";
 import AppLayout from "./components/layout/AppLayout";
-import HomePage from "./components/pages/HomePage";
+import HomePage from "./pages/HomePage";
 import CategoryLayout from "./components/layout/CategoryLayout";
-import CategoryPageTemplate from "./components/templates/CategoryPageTemplate";
-import { getCategoryNames } from "./utils/product";
-import { CategoryName } from "./models/Product";
+import CategoryPageTemplate from "./components/templates/CategoryTemplate";
+import { getAllCategorySlugsWithNames, getAllProductSlugsWithIds } from "./utils/product";
+import ProductLayout from "./components/layout/ProductLayout";
+import ProductPageTemplate from "./components/templates/ProductTemplate";
 
 function App() {
-  const categoryNames: CategoryName[] = getCategoryNames();
+  const allCategorySlugsWithNames: { name: string, slug: string }[] = getAllCategorySlugsWithNames();
+
+  const allProductSlugsWithIds: { id: number, slug: string }[] = getAllProductSlugsWithIds();
 
   const router = createBrowserRouter(
     createRoutesFromElements(
@@ -17,10 +20,16 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="/category" element={<CategoryLayout />}>
           <Route index element={<Navigate to="/" />} />
-          {categoryNames.map((catName, index) => (
-            <Route key={index} path={catName} element={<CategoryPageTemplate
-              categoryName={catName} />} />
+          {allCategorySlugsWithNames.map((cat, index) => (
+            <Route key={index} path={cat.slug}
+              element={<CategoryPageTemplate categoryName={cat.name} />}
+            />
           ))}
+        </Route>
+        <Route path="/product" element={<ProductLayout />}>
+          {allProductSlugsWithIds.map((prod, index) =>
+            <Route key={index} path={prod.slug}
+              element={<ProductPageTemplate productId={prod.id} />} />)}
         </Route>
       </Route>
     )
