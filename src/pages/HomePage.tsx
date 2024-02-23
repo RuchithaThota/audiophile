e50b4eabdae5589fe7e4768a7533303aa846a619
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-23 22:05:26 +0530

    add:HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+import { HomeData } from "../models/Product";
+import { getHomeData } from "../utils/product";
+import About from "../components/organisms/About";
+import CategoryList from "../components/organisms/CategoryList";
+import FeaturedProduct from "../components/organisms/Home/FeaturedProduct";
+import Section1 from "../components/organisms/Home/Section1";
+import Section2 from "../components/organisms/Home/Section2";
+import Section3 from "../components/organisms/Home/Section3";
+
+
+const HomePage = () => {
+  const data: HomeData[] = getHomeData();
+  return (
+    <div className="bg-secondary">
+      <FeaturedProduct product={data[0]} />
+      <div className="page-container">
+        <CategoryList />
+        <div className="flex flex-col gap-6 md:gap-8 lg:gap-12">
+          <Section1 product={data[1]} />
+          <Section2 product={data[2]} />
+          <Section3 product={data[3]} />
+        </div>
+        <About />
+      </div>
+    </div>);
+};
+
+export default HomePage;
+
