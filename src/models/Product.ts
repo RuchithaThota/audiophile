0881d4d93273ba552728455d72a7e61f77c753b8
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-23 22:04:16 +0530

    add:Product interface & RecommendedProduct type

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,6 +1,16 @@
 export type ImageAsset = { url: string };
 export type ProductImage = { mobile: string; tablet: string; desktop: string };
-export type CategoryName = string;
+export type RecommendedProduct = {
+  slug: string;
+  name: string;
+  image: ProductImage;
+};
+export type BoxItem = { quantity: number; item: string };
+export type ProductGallery = {
+  first: ProductImage;
+  second: ProductImage;
+  third: ProductImage;
+};
 export interface HomeData {
   id: number;
   slug: string;
@@ -22,4 +32,11 @@ export interface CategoryProduct {
   image: ProductImage;
   isNew: boolean;
   slug: string;
+}
+export interface Product extends CategoryProduct {
+  recommended: RecommendedProduct[];
+  features: string;
+  boxItems: BoxItem[];
+  price: number;
+  gallery: ProductGallery;
 }
