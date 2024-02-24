a042057d79765d40a7fb68d6e26b55c5724f8ccf
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-24 17:20:36 +0530

    modulate template

--- src/components/templates/CategoryTemplate.tsx
+++ src/components/templates/CategoryTemplate.tsx
@@ -1,6 +1,10 @@
 import { CategoryProduct } from "../../models/Product";
 import { getCategoryProductsByName } from "../../utils/product"
+import NewProduct from "../atoms/Product/NewProduct";
+import ProductDesc from "../atoms/Product/ProductDesc";
+import ProductName from "../atoms/Product/ProductName";
 import ProductLink from "../molecules/ProductLink";
+import ProductImageContainer from "../organisms/Product/ProductImageContainer";
 
 interface CategoryPageTemplateProps {
     categoryName: string
@@ -13,40 +17,14 @@ function CategoryPageTemplate({ categoryName }: CategoryPageTemplateProps): JSX.
         <div className="flex flex-col gap-28 md:gap-[10rem]">
             {catProducts.map((catProduct, index) => {
                 return <div className="lg:flex lg:items-center" key={index}>
-                    <img
-                        src={catProduct.image.mobile}
-                        alt={catProduct.name}
-                        loading="lazy"
-                        className="rounded-lg mb-8 md:hidden"
-                    />
-                    <img
-                        src={catProduct.image.tablet}
-                        alt={catProduct.name}
-                        loading="lazy"
-                        className="hidden md:block md:mb-[52px] md:rounded-lg lg:hidden"
-                    />
-                    <img
-                        src={catProduct.image.mobile}
-                        alt={catProduct.name}
-                        loading="lazy"
-                        className={`hidden lg:block lg:rounded-lg lg:w-1/2 
-                        ${(index + 1) % 2 === 0 ? 'lg:order-2' : ''}`}
-                    />
-                    <div className={`w-full flex flex-col items-center gap-6 
-                    text-center md:w-[572px] md:mx-auto md:my-0 
-                    lg:items-start lg:text-left lg:w-1/2  
+                    <ProductImageContainer image={catProduct.image} name={catProduct.name} index={index} />
+                    <div className={`productCard  
                     ${(index + 1) % 2 === 0 ? 'lg:pr-32' : 'lg:pl-32'}`}>
                         {catProduct.isNew &&
-                            <p className="new text-primary">
-                                New Product
-                            </p>}
-                        <h2 className="font-bold text-[28px] md:text-[40px] 
-                        text-black uppercase md:w-[360px]">
-                            {catProduct.name}
-                        </h2>
-                        <p className="font-medium text-[15px]/7 text-medium-gray">
-                            {catProduct.description}
-                        </p>
+                            <NewProduct textPrimary={true} />
+                        }
+                        <ProductName name={catProduct.name} />
+                        <ProductDesc description={catProduct.description} />
                         <ProductLink productSlug={catProduct.slug} />
                     </div>
                 </div>
--- src/components/templates/ProductTemplate.tsx
+++ src/components/templates/ProductTemplate.tsx
@@ -1,7 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import { ProductGallery, RecommendedProduct } from "../../models/Product";
 import { getProductById } from "../../utils/product"
+import NewProduct from "../atoms/Product/NewProduct";
+import ProductDesc from "../atoms/Product/ProductDesc";
+import ProductName from "../atoms/Product/ProductName";
 import Gallery from "../organisms/Product/Gallery";
 import ProductDetails from "../organisms/Product/ProductDetails";
+import ProductImageContainer from "../organisms/Product/ProductImageContainer";
 import RecommedProducts from "../organisms/Product/RecommedProducts"
 
 interface ProductPageTemplateProps {
@@ -12,13 +17,30 @@ function ProductPageTemplate({ productId }: ProductPageTemplateProps) {
     const product = getProductById(productId);
     const recommendedProducts: RecommendedProduct[] | undefined = product?.recommended;
     const productGallery: ProductGallery | undefined = product?.gallery;
+    const navigate = useNavigate();
     return (
-        <>
-            <ProductDetails />
-            {productGallery && <Gallery gallery={productGallery} />}
-            {recommendedProducts?.length &&
-                <RecommedProducts recommended={recommendedProducts} />}
-        </>
+        <div className="mt-[96px] md:mt-10">
+            <button className="text-para-secondary text-[15px] font-medium  w-fit"
+                onClick={() => {
+                    navigate(-1)
+                }}>Go Back</button>
+            <div className="mt-6 lg:mt-14 flex flex-col gap-[7.5rem] md:gap-[6rem] lg:gap-[10rem]">
+                {product && <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-24">
+                    <ProductImageContainer image={product?.image} name={product.name} fromProductTemplate={true} />
+                    <div>
+                        {product.isNew && <NewProduct textPrimary={true} />}
+                        <div className="py-6 ">
+                            <ProductName name={product.name} />
+                        </div>
+                        <ProductDesc description={product.description} />
+                    </div>
+                </div>}
+                {product && <ProductDetails features={product?.features} boxItems={product?.boxItems} />}
+                {productGallery && <Gallery gallery={productGallery} />}
+                {recommendedProducts?.length &&
+                    <RecommedProducts recommended={recommendedProducts} />}
+            </div>
+        </div>
     )
 }
 
