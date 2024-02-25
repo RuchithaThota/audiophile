96b679f763b531ac266be3a8969aa87ea57b0bad
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-25 13:33:12 +0530

    add:ProductCart component

---
+++ src/components/molecules/ProductCart.tsx
@@ -0,0 +1,17 @@
+
+function ProductCart() {
+    const quantity: number = 1;
+    return (
+        <div className="flex flex-col flex-wrap gap-4 md:flex-row ">
+            <div className="bg-lightGray w-28 h-12 px-4 
+            flex justify-between items-center font-bold">
+                <button className="text-qtyBtn">-</button>
+                <p>{quantity}</p>
+                <button className="text-qtyBtn">+</button>
+            </div>
+            <button className="primary-btn">ADD TO CART</button>
+        </div>
+    )
+}
+
+export default ProductCart
