7cc176cf94656fee0b6af486ddf6f9fff8468862
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-25 19:02:41 +0530

    add:MobileMenu

--- src/components/molecules/MobileMenu.tsx
+++ src/components/molecules/MobileMenu.tsx
@@ -2,11 +2,11 @@ import CategoryList from "../organisms/CategoryList"
 
 function MobileMenu() {
     return (
-        <div
-            className="bg-white w-full h-[calc(100%_-_5.625rem)] 
-            fixed top-[5.625rem] z-10 overflow-auto py-8 px-6 md:h-fit 
-            md:pt-14 md:pb-16 md:px-10 md:rounded-e-lg">
-            <CategoryList />
+        <div className="modal-wrapper lg:hidden
+         ">
+            <div className="bg-white py-8 px-6 md:pt-14 md:pb-16 md:px-10">
+                <CategoryList />
+            </div>
         </div>
     )
 }
