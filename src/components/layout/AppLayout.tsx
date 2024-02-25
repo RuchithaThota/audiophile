502a683f0de1683eb49159e7dd997467b405da55
RuchithaThota <ruchithathota8686@gmail.com>
2024-02-25 19:02:29 +0530

    add:MobileMenu & ShoppingCart component imports

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
+// import ShoppingCart from "../organisms/ShoppingCart";
+// import MobileMenu from "../molecules/MobileMenu";
 
 function AppLayout() {
   return (
     <div>
       <Header />
+      {/* <MobileMenu /> */}
+      {/* <ShoppingCart /> */}
       <Outlet />
       <Footer />
     </div>
