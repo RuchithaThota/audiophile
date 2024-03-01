f996a4cbad6c2c9cc3cd866bee00703cedbd3dca
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-01 23:15:15 +0530

    add:toggleCartModal

--- src/app/components/atoms/header/CartIcon.tsx
+++ src/app/components/atoms/header/CartIcon.tsx
@@ -1,6 +1,15 @@
+import { useModal } from "../../../../context/ModalContext";
+
 function CartIcon(): JSX.Element {
+  const { setIsCartModalOpen, isNavOpen, setIsNavOpen } = useModal();
+  const toggleCartModal = () => {
+    setIsCartModalOpen(prevState => !prevState);
+    if (isNavOpen)
+      setIsNavOpen(false);
+  };
   return (
-    <button className="absolute right-6 md:right-0">
+    <button className="absolute right-6 md:right-0"
+      onClick={toggleCartModal}>
       <img src="/images/shared/desktop/icon-cart.svg" alt="cartIcon" />
     </button>
   );
