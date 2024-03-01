b01d813669b261ca53c0e998631f59c239786933
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-01 23:15:37 +0530

    add:handleMenuClick function

--- src/app/components/atoms/header/MenuIcon.tsx
+++ src/app/components/atoms/header/MenuIcon.tsx
@@ -1,7 +1,16 @@
+import { useModal } from "../../../../context/ModalContext";
+
 function MenuIcon(): JSX.Element {
+  const { isCartModalOpen, setIsCartModalOpen, setIsNavOpen } = useModal();
+  function handleMenuClick() {
+    setIsNavOpen(prevState => !prevState)
+    if (isCartModalOpen)
+      setIsCartModalOpen(false);
+  }
   return (
-    <button className="absolute left-6 md:relative md:mr-10 md:left-0 lg:hidden">
-      <img src={"/images/shared/tablet/icon-hamburger.svg"} alt="menuIcon" />
+    <button className="absolute left-6 md:relative md:mr-10 md:left-0 lg:hidden"
+      onClick={handleMenuClick}>
+      <img src={"/images/shared/tablet/icon-hamburger.svg"} alt="menuIcon" className="menuIcon" />
     </button>
   );
 }
