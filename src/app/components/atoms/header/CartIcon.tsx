3580ff1118f58ce24be4ca92707701d92e57756a
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-05 14:58:15 +0530

    add:cartIcon banner

--- src/app/components/atoms/header/CartIcon.tsx
+++ src/app/components/atoms/header/CartIcon.tsx
@@ -1,7 +1,10 @@
 import { useModal } from "../../../../context/ModalContext";
+import { cartItems } from "../../../../store/cartSlice";
+import { useAppSelector } from "../../../../store/hooks";
 
 function CartIcon(): JSX.Element {
   const { setIsCartModalOpen, isNavOpen, setIsNavOpen } = useModal();
+  const cartList = useAppSelector(cartItems);
   const toggleCartModal = () => {
     setIsCartModalOpen(prevState => !prevState);
     if (isNavOpen)
@@ -11,6 +14,7 @@ function CartIcon(): JSX.Element {
     <button className="absolute right-6 md:right-0"
       onClick={toggleCartModal}>
       <img src="/images/shared/desktop/icon-cart.svg" alt="cartIcon" />
+      <div className="absolute top-[-10px] right-[-12px] bg-primary w-[20px] h-[20px] rounded-xl text-white font-bold text-sm text-center">{cartList.length}</div>
     </button>
   );
 }
