0fdde3253e4fbe7e9e425c5d90ac5d8fa72be73a
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-05 14:10:50 +0530

    add dynamic values

--- src/app/components/organisms/checkout/CartSummary.tsx
+++ src/app/components/organisms/checkout/CartSummary.tsx
@@ -1,18 +1,25 @@
+import { SHIPPING_FEE } from "../../../constants/fee";
+import useCartTotal from "../../../hooks/useCartTotal";
+import { formatToUSD } from "../../../utils/formatterUSD";
 import AmountItem from "../../atoms/checkout/AmountItem";
 import CartList from "../../molecules/CartList";
 
 function CartSummary() {
+    const { cartTotal, gst, grandTotal } = useCartTotal();
+    const [cartTotalUSD, ShippingFeeUSD, gstUSD, grandTotalUSD] =
+        formatToUSD([cartTotal, SHIPPING_FEE, gst, grandTotal]);
+    //JSX
     return (
-        <div className="w-full lg:w-[24.875rem] bg-white mt-8 lg:mt-0 px-6 py-8 md:p-8 rounded-lg
-        flex flex-col gap-8">
+        <div className="w-full lg:w-[30.875rem] bg-white mt-8 lg:mt-0 px-6 
+        py-8 md:p-8 rounded-lg flex flex-col gap-8">
             <h1 className="h1 text-[18px]">Summary</h1>
-            <CartList />
+            <CartList fromCartSummary={true} />
             <div>
-                <AmountItem label="Total" amount={"$2000"} />
-                <AmountItem label="Shipping" amount={"$50"} />
-                <AmountItem label="GST(included)" amount={"$500"} />
+                <AmountItem label="Total" amount={cartTotalUSD} />
+                <AmountItem label="Shipping" amount={ShippingFeeUSD} />
+                <AmountItem label="GST(included)" amount={gstUSD} />
                 <div className="mt-4">
-                    <AmountItem label="Grand Total" amount={"$2550"} />
+                    <AmountItem label="Grand Total" amount={grandTotalUSD} />
                 </div>
             </div>
             <button className="primary-btn w-full justify-center">Continue and Pay</button>
