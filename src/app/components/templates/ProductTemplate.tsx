b0f951c96db8def8a1c22d1cf73705b4bcb05816
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-05 14:19:30 +0530

    refactor

--- src/app/components/templates/ProductTemplate.tsx
+++ src/app/components/templates/ProductTemplate.tsx
@@ -9,21 +9,25 @@ import ProductImageContainer from "../organisms/product/ProductImageContainer";
 import RecommedProducts from "../organisms/product/RecommedProducts"
 import ProductCart from "../molecules/ProductCart";
 import GoBackLink from "../atoms/GoBackLink";
+import { formatToUSD } from "../../utils/formatterUSD";
+import { useMemo } from "react";
 
 interface ProductPageTemplateProps {
     productId: number
 }
 
 function ProductPageTemplate({ productId }: ProductPageTemplateProps) {
-    const product = getProductById(productId);
-    const recommendedProducts: RecommendedProduct[] | undefined = product?.recommended;
-    const productGallery: ProductGallery | undefined = product?.gallery;
+    const product = useMemo(() => getProductById(productId), [productId]);
+    if (!product) return null;
+    const [productPriceUSD] = formatToUSD([product.price]);
+    const recommendedProducts: RecommendedProduct[] = product.recommended;
+    const productGallery: ProductGallery = product.gallery;
     return (
         <div className="mt-[96px] md:mt-10">
             <GoBackLink />
             <div className="mt-6 lg:mt-14 flex flex-col gap-[7.5rem] md:gap-[6rem] lg:gap-[10rem]">
-                {product && <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-24">
-                    <ProductImageContainer image={product?.image} name={product.name} fromProductTemplate={true} />
+                <div className="flex flex-col items-center md:flex-row md:justify-center md:gap-24">
+                    <ProductImageContainer image={product.image} name={product.name} fromProductTemplate={true} />
                     <div>
                         {product.isNew && <NewProduct textPrimary={true} />}
                         <div className="py-6 ">
@@ -31,13 +35,14 @@ function ProductPageTemplate({ productId }: ProductPageTemplateProps) {
                         </div>
                         <ProductDesc description={product.description} />
                         <p className="h2 text-black text-lg pt-6 pb-8 md:py-8">
-                            $ {product?.price}</p>
-                        <ProductCart />
+                            {productPriceUSD}</p>
+                        <ProductCart product={product} />
                     </div>
-                </div>}
-                {product && <ProductDetails features={product?.features} boxItems={product?.boxItems} />}
+                </div>
+                <ProductDetails features={product.features}
+                    boxItems={product.boxItems} />
                 {productGallery && <Gallery gallery={productGallery} />}
-                {recommendedProducts?.length &&
+                {recommendedProducts.length > 0 &&
                     <RecommedProducts recommended={recommendedProducts} />}
             </div>
         </div>
