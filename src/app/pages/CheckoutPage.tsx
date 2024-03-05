801ee7c96324f27c2bec40d16aaa4df297021bef
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-05 21:47:14 +0530

    add:FormContextProvider

--- src/app/pages/CheckoutPage.tsx
+++ src/app/pages/CheckoutPage.tsx
@@ -1,3 +1,4 @@
+import FormContextProvider from "../../context/FormContext"
 import GoBackLink from "../components/atoms/GoBackLink"
 import CartSummary from "../components/organisms/checkout/CartSummary"
 import Checkout from "../components/organisms/checkout/Checkout"
@@ -5,19 +6,21 @@ import ConfirmOrder from "../components/organisms/checkout/ConfirmOrder"
 
 function CheckoutPage() {
     return (
-        <div className="relative">
-            <div className="w-full bg-customGray mt-[5.625rem] pt-4 px-6 pb-[5.625rem]
+        <FormContextProvider>
+            <div className="relative">
+                <div className="w-full bg-customGray mt-[5.625rem] pt-4 px-6 pb-[5.625rem]
         md:pt-[48px] md:px-[40px] md:pb-[116px] lg:pt-[79px] lg:pb-[141px]">
-                <div className="max-w-[69.375rem] mx-auto flex flex-col ">
-                    <GoBackLink />
-                    <div className="mt-6 lg:mt-14 lg:flex lg:gap-[1.875rem] lg:items-start">
-                        <Checkout />
-                        <CartSummary />
+                    <div className="max-w-[69.375rem] mx-auto flex flex-col ">
+                        <GoBackLink />
+                        <div className="mt-6 lg:mt-14 lg:flex lg:gap-[1.875rem] lg:items-start">
+                            <Checkout />
+                            <CartSummary />
+                        </div>
                     </div>
                 </div>
+                <ConfirmOrder />
             </div>
-            <ConfirmOrder />
-        </div>
+        </FormContextProvider>
     )
 }
 
