ab47f134e5c2686441b8f5e6881312bae8b858a0
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-05 14:12:42 +0530

    add dynamic functionality

--- src/app/components/molecules/ProductCart.tsx
+++ src/app/components/molecules/ProductCart.tsx
@@ -1,15 +1,50 @@
+import { useState } from "react"
+import { useAppDispatch } from "../../../store/hooks";
+import { addItemToCart } from "../../../store/cartSlice";
+import { Product } from "../../models/Product";
+import CartItem from "../../models/Cart";
+import { useToast } from "../../../context/ToastContext";
 
-function ProductCart() {
-    const quantity: number = 1;
+interface ProductCartProps {
+    product: Product
+}
+
+function ProductCart({ product }: ProductCartProps) {
+    const dispatch = useAppDispatch();
+    const { showToast } = useToast();
+    const [qty, setQty] = useState(1);
+    const item: CartItem = {
+        id: product.id,
+        image: product.cartImage,
+        name: product.shortName,
+        price: product.price,
+        quantity: qty
+    }
+    const increaseQty = () => {
+        setQty(prev => prev + 1)
+    }
+    const decreaseQty = () => {
+        setQty(prev => Math.max(prev - 1, 1))
+    }
+    const handleAddToCartClick = () => {
+        dispatch(addItemToCart(item))
+        setQty(1);
+        showToast({ type: 'success', title: `${product.shortName} added to the cart!`, duration: 1000 })
+    }
+    //JSX
     return (
         <div className="flex flex-col flex-wrap gap-4 md:flex-row ">
             <div className="bg-customGray w-28 h-12 px-4 
             flex justify-between items-center font-bold">
-                <button className="text-qtyBtn">-</button>
-                <p>{quantity}</p>
-                <button className="text-qtyBtn">+</button>
+                <button className="qtyBtn"
+                    onClick={decreaseQty}>-</button>
+                <p>{qty}</p>
+                <button className="qtyBtn"
+                    onClick={increaseQty}
+                >+</button>
             </div>
-            <button className="primary-btn">ADD TO CART</button>
+            <button className="primary-btn"
+                onClick={handleAddToCartClick}>ADD TO CART</button>
         </div>
     )
 }
