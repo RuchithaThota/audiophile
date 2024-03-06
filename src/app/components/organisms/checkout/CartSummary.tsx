e5a55849429028a202f31710b3993da8ef84ddff
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-06 13:39:54 +0530

    add:handleContinueAndPay function

--- src/app/components/organisms/checkout/CartSummary.tsx
+++ src/app/components/organisms/checkout/CartSummary.tsx
@@ -1,15 +1,67 @@
+import { useForm } from "../../../../context/FormContext";
+import { useModal } from "../../../../context/ModalContext";
 import { SHIPPING_FEE } from "../../../constants/fee";
+import { EMAIL_REGEX } from "../../../constants/regex";
 import useCartTotal from "../../../hooks/useCartTotal";
 import { formatToUSD } from "../../../utils/formatterUSD";
 import AmountItem from "../../atoms/checkout/AmountItem";
 import CartList from "../../molecules/CartList";
 
 function CartSummary() {
+    const { setFormErrors, formData, resetFormData } = useForm();
+    const { setIsConfirmModalOpen } = useModal();
     const { cartTotal, gst, grandTotal } = useCartTotal();
     const [cartTotalUSD, ShippingFeeUSD, gstUSD, grandTotalUSD] =
         formatToUSD([cartTotal, SHIPPING_FEE, gst, grandTotal]);
-    const handleContinueAndPay = () => { 
-        
+    //handleContinueAndPay
+    const handleContinueAndPay = () => {
+        const errors: { [key: string]: string } = {};
+        if (formData.name === '') {
+            errors.name = "Name is required";
+        }
+        if (formData.email === '') {
+            errors.email = "Email is required";
+        } else if (!EMAIL_REGEX.test(formData.email)) {
+            errors.email = "Invalid email address";
+        }
+        if (formData.phoneNumber === '') {
+            errors.phoneNumber = "Phone number is required";
+        } else if (formData.phoneNumber.length !== 10) {
+            errors.phoneNumber = "Invalid phone number"
+        }
+        if (formData.address === '') {
+            errors.address = "Address is required";
+        }
+        if (formData.pincode === '') {
+            errors.pincode = "Pincode is required";
+        } else if (formData.pincode.length !== 6) {
+            errors.pincode = "Pincode must be 6 digits"
+        }
+        if (formData.city === '') {
+            errors.city = "City is required";
+        }
+        if (formData.country === '') {
+            errors.country = "Country is required";
+        }
+        if (formData.paymentType === '') {
+            errors.paymentType = "Payment type is required";
+        }
+        if (formData.paymentType === 'Online' && formData.eNumber === '') {
+            errors.eNumber = "e-Money number is required";
+        } else if (formData.eNumber.length !== 9) {
+            errors.eNumber = 'Wrong Format'
+        }
+        if (formData.paymentType === 'Online' && formData.ePin === '') {
+            errors.ePin = "e-Money PIN is required";
+        } else if (formData.ePin.length !== 4) {
+            errors.ePin = "Wrong Format";
+        }
+        setFormErrors(errors);
+        const hasErrors = Object.keys(errors).length > 0;
+        if (!hasErrors) {
+            setIsConfirmModalOpen(true);
+            resetFormData();
+        }
     }
     //JSX
     return (
