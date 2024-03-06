d6da71f3df46a6b598c2c1038b29e3f262a268fe
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-06 13:41:31 +0530

    change:handleChange functionality

--- src/app/components/atoms/checkout/TextInput.tsx
+++ src/app/components/atoms/checkout/TextInput.tsx
@@ -1,4 +1,5 @@
 import { useForm } from "../../../../context/FormContext";
+import ErrorField from "./ErrorField";
 
 interface TextInputProps {
     label: string;
@@ -8,18 +9,29 @@ interface TextInputProps {
 }
 
 const TextInput: React.FC<TextInputProps> = ({ label, name, placeholder, value }) => {
-    const { setFormData } = useForm();
+    const { setFormData, formErrors } = useForm();
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setFormData(prevFormData => ({ ...prevFormData, [e.target.name]: e.target.value }))
+        const name = e.target.name;
+        let value = e.target.value;
+        if (name === 'eNumber' || name === 'ePin' || name === 'pincode' ||
+            name === "phoneNumber") {
+            value = value.replace(/\D/g, '');
+        } else {
+            value = value.replace(/ +/g, ' ');
+        }
+        setFormData(prevFormData => ({ ...prevFormData, [name]: value }))
     }
     return (
         <div>
-            <label className="text-[12px] flex mb-3 font-bold">{label}</label>
+            <div className="flex justify-between items-center">
+                <label className="text-[12px] flex mb-3 font-bold">{label}</label>
+                <ErrorField errorMsg={formErrors[name]} />
+            </div>
             <input type="text" placeholder={placeholder} name={name}
                 value={value}
                 onChange={handleChange}
-                className="w-full h-14 border-[0.0625rem] border-input px-6 
-                rounded-lg font-medium  text-[13px] outline-none focus:border-primary placeholder:text-qtyBtn" />
+                className={`w-full h-14 border-[0.0625rem] border-input px-6 
+                rounded-lg font-medium  text-[13px] outline-none focus:border-primary placeholder:text-qtyBtn ${formErrors[name] ? "border-red-500" : null}`} />
         </div>
     )
 }
