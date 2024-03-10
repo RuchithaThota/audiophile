73da6a726ad1bc045ab85f50a08d38e240d23a14
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-10 17:17:19 +0530

    fix:ProfileIcon styles

--- src/app/components/atoms/ProfileIcon.tsx
+++ src/app/components/atoms/ProfileIcon.tsx
@@ -3,7 +3,7 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 function ProfileIcon(): JSX.Element {
     return (
         <button className='absolute cursor-pointer 
-        top-[28px] right-[54px] md:right-[38px]'>
+        top-[28px] right-[60px] md:right-[38px]'>
             <PersonOutlineOutlinedIcon
                 style={{
                     color: "white",
