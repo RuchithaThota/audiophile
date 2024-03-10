a75a967ee18f8b415243eac2cafd61d9464bb662
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-10 17:12:01 +0530

    firebase config initiate

---
+++ src/firebase/index.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAmxQ8QxnUlvorExKM8ZWs9X1rdHIctSlY",
+  authDomain: "audiophile-b7e5c.firebaseapp.com",
+  projectId: "audiophile-b7e5c",
+  storageBucket: "audiophile-b7e5c.appspot.com",
+  messagingSenderId: "815213882882",
+  appId: "1:815213882882:web:4c227b7be891f16bc9011b",
+  measurementId: "G-M5WW2RJ8FN",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
