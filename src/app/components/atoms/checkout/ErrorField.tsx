d4acccb2bac0d742fe8198368ab63959683dafe0
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 12:14:32 +0530

    fix:styles

--- src/app/components/atoms/checkout/ErrorField.tsx
+++ src/app/components/atoms/checkout/ErrorField.tsx
@@ -2,7 +2,8 @@
 function ErrorField({ errorMsg }: { errorMsg: string }) {
     return (
         <div className="mb-3 mr-2">
-            <p className="text-red-500 font-medium text-[10px] md:text-[12px]">{errorMsg}</p>
+            <p className="text-red-500 font-medium text-[10px] md:text-[12px]">
+                {errorMsg}</p>
         </div>
     )
 }
