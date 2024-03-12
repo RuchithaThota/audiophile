674c3f37cf492e6dbedc58a910bb5c92b257cba1
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 14:03:13 +0530

    currentUser

--- src/app/components/atoms/ProfileIcon.tsx
+++ src/app/components/atoms/ProfileIcon.tsx
@@ -1,12 +1,24 @@
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { currentUser } from '../../../firebase/AuthService';
+import { useToast } from '../../../context/ToastContext';
 
 function ProfileIcon(): JSX.Element | null {
     const { pathname } = useLocation();
+    const navigate = useNavigate();
+    const user = currentUser();
+    const { showToast } = useToast();
     if (pathname === "/login") return null;
+    const handleProfileIconClick = () => {
+        if (!user) {
+            navigate("/login")
+        } else {
+            if (user.displayName)
+                showToast({ type: 'success', title: user.displayName, duration: 1000 })
+        }
+    }
     return (
-        <Link
-            to="/login"
+        <div
             className='absolute cursor-pointer 
         top-[28px] right-[60px] md:right-[50px]'>
             <PersonOutlineOutlinedIcon
@@ -14,8 +26,9 @@ function ProfileIcon(): JSX.Element | null {
                     color: "white",
                     fontSize: "2rem"
                 }}
+                onClick={handleProfileIconClick}
             />
-        </Link>
+        </div>
     )
 }
 
