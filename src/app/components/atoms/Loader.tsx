bc0d25ae5d90fa7c57fb5fdab477b1716bfc8ed9
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 20:28:26 +0530

    fix:Loader styles

--- src/app/components/atoms/Loader.tsx
+++ src/app/components/atoms/Loader.tsx
@@ -3,11 +3,12 @@ import { CircularProgress } from '@mui/material'
 function Loader() {
     return (
         <div className="
-                bg-blur w-[40px] h-[40px] rounded-full 
+                bg-white w-[40px] h-[40px] rounded-full 
                 flex-center
                 z-10
                 absolute-left-center
                 top-[50%]
+                shadow-lg
                 ">
             <CircularProgress size={20} style={{
                 color: "#d87d4a"
