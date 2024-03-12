afb420dd74e66abd5ec7c3a41de7726ee8b2ba57
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 20:29:13 +0530

    handle modal states

--- src/app/components/atoms/header/MenuIcon.tsx
+++ src/app/components/atoms/header/MenuIcon.tsx
@@ -1,11 +1,13 @@
 import { useModal } from "../../../../context/ModalContext";
 
 function MenuIcon(): JSX.Element {
-  const { isCartModalOpen, setIsCartModalOpen, setIsNavOpen } = useModal();
+  const { isCartModalOpen, setIsCartModalOpen, setIsNavOpen, setShowProfile, showProfile } = useModal();
   function handleMenuClick() {
     setIsNavOpen(prevState => !prevState)
     if (isCartModalOpen)
       setIsCartModalOpen(false);
+    if (showProfile)
+      setShowProfile(false);
   }
   return (
     <button className="absolute left-6 md:relative md:mr-10 md:left-0 lg:hidden"
