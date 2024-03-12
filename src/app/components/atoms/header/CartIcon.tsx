d2886d75b1aa0e464321b3ec079533a3ad1e01f0
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 20:29:03 +0530

    handle modal states

--- src/app/components/atoms/header/CartIcon.tsx
+++ src/app/components/atoms/header/CartIcon.tsx
@@ -3,12 +3,14 @@ import { cartItems } from "../../../../store/cartSlice";
 import { useAppSelector } from "../../../../store/hooks";
 
 function CartIcon(): JSX.Element {
-  const { setIsCartModalOpen, isNavOpen, setIsNavOpen } = useModal();
+  const { setIsCartModalOpen, isNavOpen, setIsNavOpen, setShowProfile, showProfile } = useModal();
   const cartList = useAppSelector(cartItems);
   const toggleCartModal = () => {
     setIsCartModalOpen(prevState => !prevState);
     if (isNavOpen)
       setIsNavOpen(false);
+    if (showProfile)
+      setShowProfile(false)
   };
   return (
     <button className="absolute right-6  md:right-0"
