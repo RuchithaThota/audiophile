cde51985d5d888bb5c2ad114456c5669647c97ac
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 16:57:51 +0530

    add:isLoading contextType

--- src/context/ModalContext.tsx
+++ src/context/ModalContext.tsx
@@ -10,7 +10,11 @@ type ContextType = {
     isNavOpen: boolean,
     setIsNavOpen: React.Dispatch<React.SetStateAction<boolean>>,
     isConfirmModalOpen: boolean,
-    setIsConfirmModalOpen: React.Dispatch<React.SetStateAction<boolean>>
+    setIsConfirmModalOpen: React.Dispatch<React.SetStateAction<boolean>>,
+    isLoading: boolean,
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+    showProfile: boolean,
+    setShowProfile: React.Dispatch<React.SetStateAction<boolean>>
 }
 const initialState = {
     isCartModalOpen: false,
@@ -18,7 +22,11 @@ const initialState = {
     isNavOpen: false,
     setIsNavOpen: () => { },
     isConfirmModalOpen: false,
-    setIsConfirmModalOpen: () => { }
+    setIsConfirmModalOpen: () => { },
+    isLoading: false,
+    setIsLoading: () => { },
+    showProfile: false,
+    setShowProfile: () => { }
 }
 const ModalContext = createContext<ContextType>(initialState);
 
@@ -28,8 +36,11 @@ export const useModal = (): ContextType => useContext(ModalContext);
 const ModalContextProvider: React.FC<ModalContextProviderProps> = ({ children }) => {
     const [isCartModalOpen, setIsCartModalOpen] = useState(initialState.isCartModalOpen);
     const [isNavOpen, setIsNavOpen] = useState(initialState.isNavOpen)
-    const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(initialState.isConfirmModalOpen)
-    const contextValue: ContextType = { isCartModalOpen, setIsCartModalOpen, isNavOpen, setIsNavOpen, isConfirmModalOpen, setIsConfirmModalOpen }
+    const [isConfirmModalOpen, setIsConfirmModalOpen] =
+        useState(initialState.isConfirmModalOpen)
+    const [isLoading, setIsLoading] = useState(initialState.isLoading)
+    const [showProfile, setShowProfile] = useState(initialState.showProfile);
+    const contextValue: ContextType = { isCartModalOpen, setIsCartModalOpen, isNavOpen, setIsNavOpen, isConfirmModalOpen, setIsConfirmModalOpen, setIsLoading, isLoading, showProfile, setShowProfile }
     return <ModalContext.Provider value={contextValue}>
         {children}
     </ModalContext.Provider>
