2275bb15a9858840c3706d0e4871e6b16a4f91f4
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 16:57:37 +0530

    refactor:LoginPage

--- src/app/pages/LoginPage.tsx
+++ src/app/pages/LoginPage.tsx
@@ -10,17 +10,13 @@ function LoginPage() {
     const [confirmResult, setConfirmResult] = useState<ConfirmationResult | undefined>(undefined);
     return (
         <div className="bg-customGray w-full h-[100vh] 
-        p-4 mt-[5.625rem] fixed">
-            <div className="bg-white w-full max-w-[400px] 
-            mt-[40px] lg:mt-[80px] mx-auto
-            rounded-lg p-6 md:p-10 flex flex-col gap-6">
-                {!showOtpScreen && <Login
-                    setConfirmResult={setConfirmResult}
-                    setShowOtpScreen={setShowOtpScreen} />}
-                {showOtpScreen && <Otp
-                    setConfirmResult={setConfirmResult}
-                    confirmResult={confirmResult} />}
-            </div>
+        p-4 pt-[5.625rem] fixed">
+            {!showOtpScreen && <Login
+                setConfirmResult={setConfirmResult}
+                setShowOtpScreen={setShowOtpScreen} />}
+            {showOtpScreen && <Otp
+                setConfirmResult={setConfirmResult}
+                confirmResult={confirmResult} />}
         </div>
     )
 }
