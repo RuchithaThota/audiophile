8d971c0410800983c75b84d9a0030161b4d33af4
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 12:15:43 +0530

    feat:LoginPage component

---
+++ src/app/pages/LoginPage.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import Login from "../components/organisms/Login"
+import Otp from "../components/organisms/Otp"
+import { ConfirmationResult } from "firebase/auth";
+
+
+
+function LoginPage() {
+    const [showOtpScreen, setShowOtpScreen] = useState(false);
+    const [confirmResult, setConfirmResult] = useState<ConfirmationResult | undefined>(undefined);
+    return (
+        <div className="bg-customGray w-full h-[100vh] 
+        flex-center p-4
+        mt-[5.625rem]">
+            <div className="bg-white w-full max-w-[400px] mx-auto
+            rounded-lg p-6 md:p-10 flex flex-col gap-6">
+                {!showOtpScreen && <Login
+                    setConfirmResult={setConfirmResult}
+                    setShowOtpScreen={setShowOtpScreen} />}
+                {showOtpScreen && <Otp
+                    setConfirmResult={setConfirmResult}
+                    confirmResult={confirmResult} />}
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage
