38578eef4f896bfec8f0c6f84c27dab26d7fc2ac
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 18:04:09 +0530

    add lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import React, { Suspense } from "react";
 import {
   Navigate,
   Route, RouterProvider, createBrowserRouter, createRoutesFromElements,
 } from "react-router-dom";
-import AppLayout from "./app/components/layout/AppLayout";
-import { getAllCategorySlugsWithNames, getAllProductSlugsWithIds } from "./app/utils/product";
-import HomePage from "./app/pages/HomePage";
-import CategoryPageTemplate from "./app/components/templates/CategoryTemplate";
-import ProductPageTemplate from "./app/components/templates/ProductTemplate";
-import CheckoutPage from "./app/pages/CheckoutPage";
 import { Provider } from "react-redux";
 import { store } from "./store";
 import ModalContextProvider from "./context/ModalContext";
 import ToastContextProvider from "./context/ToastContext";
-import LoginPage from "./app/pages/LoginPage";
+import { getAllCategorySlugsWithNames, getAllProductSlugsWithIds } from "./app/utils/product";
 import PrivateRoutes from "./routes/PrivateRoutes";
+import LoginPage from "./app/pages/LoginPage";
+import Fallback from "./app/components/atoms/Fallback";
+const AppLayout = React.lazy(() => import("./app/components/layout/AppLayout"));
+const CheckoutPage = React.lazy(() => import("./app/pages/CheckoutPage"));
+const HomePage = React.lazy(() => import("./app/pages/HomePage"));
+const CategoryPageTemplate = React.lazy(() =>
+  import("./app/components/templates/CategoryTemplate"))
+const ProductPageTemplate = React.lazy(() =>
+  import("./app/components/templates/ProductTemplate"))
 
 function App() {
   const allCategorySlugsWithNames: { name: string, slug: string }[] = getAllCategorySlugsWithNames();
@@ -22,20 +26,22 @@ function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<AppLayout />}>
-        <Route index element={<HomePage />} />
+      <Route path="/" element={<Suspense fallback={<Fallback />}><AppLayout /></Suspense>}>
+        <Route index element={<Suspense fallback={<Fallback />}><HomePage /></Suspense>} />
         {allCategorySlugsWithNames.map((cat, index) => (
           <Route key={index} path={`category/${cat.slug}`}
-            element={<CategoryPageTemplate categoryName={cat.name} />}
+            element={<Suspense fallback={<Fallback />}>
+              <CategoryPageTemplate categoryName={cat.name} /></Suspense>}
           />
         ))}
         <Route path="login" element={<LoginPage />} />
         <Route element={<PrivateRoutes />}>
-          <Route path="checkout" element={<CheckoutPage />} />
+          <Route path="checkout" element={<Suspense fallback={<Fallback />}><CheckoutPage /></Suspense>} />
         </Route>
         {allProductSlugsWithIds.map((prod, index) =>
           <Route key={index} path={`product/${prod.slug}`}
-            element={<ProductPageTemplate productId={prod.id} />} />)}
+            element={<Suspense fallback={<Fallback />}>
+              <ProductPageTemplate productId={prod.id} /></Suspense>} />)}
         <Route path="*" element={<Navigate to={"/"} />} />
       </Route>
     )
@@ -50,5 +56,6 @@ function App() {
 }
 
 export default App;
+
 
 
