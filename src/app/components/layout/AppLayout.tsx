507e06c9c129619f77be6c9b52739e6910837009
RuchithaThota <ruchithathota8686@gmail.com>
2024-03-12 12:14:59 +0530

    disable Footer in LoginPage

--- src/app/components/layout/AppLayout.tsx
+++ src/app/components/layout/AppLayout.tsx
@@ -1,17 +1,18 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../organisms/Header";
 import Footer from "../organisms/Footer";
 import MobileMenu from "../molecules/MobileMenu";
 import ShoppingCart from "../organisms/ShoppingCart";
 
 function AppLayout() {
+  const { pathname } = useLocation();
   return (
     <div>
       <Header />
       <MobileMenu />
       <ShoppingCart />
       <Outlet />
-      <Footer />
+      {pathname !== "/login" && <Footer />}
     </div>
   );
 }
